220f03795e04b347a84c83c964e093319ddcf84a
Jacobloseycoder <loseyjacob1@gmail.com>
2024-08-20 13:48:03 -0500

    Create chapter2.py

---
+++ chapter2.py
@@ -0,0 +1,10 @@
+# chapter 2 practis
+def practice1():
+    #practice 1 recives no argument
+    #it outputs a message to the user
+    print("this is practis 1")
+    
+def practice2():
+    #practice 2 recives no argument
+    #it outputs a message to the user
+    print("me is king")
