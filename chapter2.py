cccfca93d24ab3212ef0e733eb50ec915c47a1f8
Jacobloseycoder <loseyjacob1@gmail.com>
2024-08-22 13:10:57 -0500

    Update chapter2.py

--- chapter2.py
+++ chapter2.py
@@ -8,3 +8,7 @@ def practice2():
     #practice 2 recives no argument
     #it outputs a message to the user
     print("me is king")
+ 
+ def program2_3()
+    print("dont fear")
+    print ("im here")
