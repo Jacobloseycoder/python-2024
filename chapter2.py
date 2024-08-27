55b544a2995d8d07abd5a019f68f661e49fba1a7
Jacobloseycoder <loseyjacob1@gmail.com>
2024-08-27 13:53:08 -0500

    Update chapter2.py

--- chapter2.py
+++ chapter2.py
@@ -82,3 +82,9 @@ def program20():
         4 =264.82
         5 = 88.08
         6 = 800.00
+        print(1)
+        print(2)
+        print(3)
+        print(4)
+        print(5)
+        print(6)
