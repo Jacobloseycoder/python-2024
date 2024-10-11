7424026e2d674077e0d9a5a537593ae44d131374
Jacobloseycoder <loseyjacob1@gmail.com>
2024-10-11 13:34:54 -0500

    Update chapter5pr.py

--- chapter5pr.py
+++ chapter5pr.py
@@ -7,4 +7,32 @@ def p2():
     print('theres only one way to win')
     
 def p3():
-    
+    def step1():
+        print('unplug the dryer')
+    def step2():
+        print('remove the 6 screws on the back')
+    def step3():
+        print('remove the back pannal')
+    def step4():
+        print('pull the top of the dryer of the top')
+    def step5():
+        print('pull the frunt off')
+    go = int(input('what step are you on 0 to leave'))
+    while go < 6:
+        if go < 1:
+            print('ending...')
+        elif go == 1:
+            step1()
+            go = go + 1
+        elif go == 2:
+            step2()
+            go = go + 1
+        elif go == 3:
+            step3()
+            go = go + 1
+        elif go == 4:
+            step4()
+            go = go + 1
+        elif go == 5:
+            step5()
+            go = go + 1
