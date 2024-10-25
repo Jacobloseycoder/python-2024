7562c97fb74644df025fa62633cc6a8d06f6dc6e
Jacobloseycoder <loseyjacob1@gmail.com>
2024-10-25 13:47:36 -0500

    Update chapter5pr.py

--- chapter5pr.py
+++ chapter5pr.py
@@ -2,6 +2,7 @@ import random
 import math
 import circle
 import rectangal
+import turtle
 
 def p1():
     print('me is iornman')
@@ -264,5 +265,29 @@ def p29():
    tot = math.hypot(AAA,BBB)
    print('the hypotenuse is',tot)
    
-def p30():
-    
+def p30(x, y, width, color):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.fillcolor(color)
+    turtle.pendown()
+    turtle.begin_fill()
+    for count in range(4):
+        turtle.forward(width)
+        turtle.left(90)
+    turtle.end_fill()
+
+def p31(x, y, radius, color):
+    turtle.penup()        
+    turtle.goto(x, y - radius) 
+    turtle.fillcolor(color)    
+    turtle.pendown()           
+    turtle.begin_fill()        
+    turtle.circle(radius)      
+    turtle.end_fill()          
+
+def p32(startX, startY, endX, endY, color):
+    turtle.penup()              
+    turtle.goto(startX, startY) 
+    turtle.pendown()            
+    turtle.pencolor(color)      
+    turtle.goto(endX, endY)
