980abc95d274f1a9494aef968e15eda54d82ab91
Jacobloseycoder <loseyjacob1@gmail.com>
2024-10-28 13:23:28 -0500

    Update chapter5pr.py

--- chapter5pr.py
+++ chapter5pr.py
@@ -3,6 +3,7 @@ import math
 import circle
 import rectangal
 import turtle
+import my_gragh
 
 def p1():
     print('me is iornman')
@@ -265,29 +266,11 @@ def p29():
    tot = math.hypot(AAA,BBB)
    print('the hypotenuse is',tot)
    
-def p30(x, y, width, color):
-    turtle.penup()
-    turtle.goto(x, y)
-    turtle.fillcolor(color)
-    turtle.pendown()
-    turtle.begin_fill()
-    for count in range(4):
-        turtle.forward(width)
-        turtle.left(90)
-    turtle.end_fill()
-
-def p31(x, y, radius, color):
-    turtle.penup()        
-    turtle.goto(x, y - radius) 
-    turtle.fillcolor(color)    
-    turtle.pendown()           
-    turtle.begin_fill()        
-    turtle.circle(radius)      
-    turtle.end_fill()          
-
-def p32(startX, startY, endX, endY, color):
-    turtle.penup()              
-    turtle.goto(startX, startY) 
-    turtle.pendown()            
-    turtle.pencolor(color)      
-    turtle.goto(endX, endY)
+def p30():
+    my_gragh.square(-100,100,100,'grey')
+    my_gragh.circle(0,100,50,'blue')
+    my_gragh.circle(-100,-100,50,'red')
+    my_gragh.circle(100,-100,50,'green')
+    #my_gragh.line()
+    #my_gragh.line()
+    #my_gragh.line()
