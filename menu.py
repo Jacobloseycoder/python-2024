1b690fc70abc286bbd1e9240134f79eb5be46e28
Jacobloseycoder <loseyjacob1@gmail.com>
2024-11-13 13:58:20 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,5 +1,4 @@
 def main():
-    choice = 0
     print('please select from the list')
     print('1. sales tax')
     print('2. calurys')
@@ -10,24 +9,5 @@ def main():
     print('7. rock,paper,scissors,lizzard,spock')
     print('8. snowman')
     print('9. checers bord')
-    choice = input('enter:')
-    if choice == 1:
-        ex1()
-    elif choice == 2:
-        ex2()
-    elif choice == 3:
-        ex3()
-    elif choice == 4:
-        ex4()
-    elif choice == 5:
-        ex5()
-    elif choice == 6:
-        ex6()
-    elif choice == 7:
-        ex7()
-    elif choice == 8:
-        ex8()
-    elif choice == 9:
-        ex9()
-    else:
-        print('ending')
+    choice = input('enter choice:')
+    return choice
