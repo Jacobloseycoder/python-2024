aef691de1e212d7f27728b6bec7cb2c5c89b3a40
Jacobloseycoder <loseyjacob1@gmail.com>
2024-11-14 14:00:33 -0600

    Update chapter5.py

--- chapter5.py
+++ chapter5.py
@@ -27,60 +27,97 @@ def main():
     else:
         print('ending')
 def ex1():
+    # Take input for the cost of the item
     amount = float(input('what was the cost'))
+    
+    # Call helper functions to calculate state tax, country tax, and total tax
     mun = state(amount)
     dun = country(amount)
     bun = tax(mun, dun)
+    
+    # Calculate total sale including taxes
     popo = total2(bun, amount)
+    
+    # Output the results
     printt(amount, mun, dun, bun, popo)
+
+# Function to calculate state tax (5% of amount)
 def state(amount):
     mun = amount * .05
     return mun
+
+# Function to calculate country tax (2.5% of amount)
 def country(amount):
     dun = amount * .025
     return dun
+
+# Function to calculate total tax (sum of state and country tax)
 def tax(mun, dun):
     bun = mun + dun
     return bun
+
+# Function to calculate total sale (amount + total tax)
 def total2(bun, amount):
     popo = bun + amount
     return popo
-def printt(amount, mun, dun, bun, popo):
-    print('the cost is',amount)
-    print('state tax is',mun)
-    print('the country tax is',dun)
-    print('the total tax is',bun)
-    print('the total sale is',popo)
 
+# Function to print out cost, taxes, and total sale
+def printt(amount, mun, dun, bun, popo):
+    print('the cost is', amount)
+    print('state tax is', mun)
+    print('the country tax is', dun)
+    print('the total tax is', bun)
+    print('the total sale is', popo)
+
+
+# Function for Exercise 2: Calculates calorie intake from carbs and fats
 def ex2():
+    # Take input for grams of carbs and fats
     cal = float(input('how many grams of carbs did you eat:'))
     caler = float(input('how many grams of fats did you eat:'))
+    
+    # Calculate calories from fats and carbs
     fat = fats(caler)
     car = carb(cal)
+    
+    # Output the calorie intake
     print('here is your calory intake')
-    print('you have consumed',fat,'amount of calorys in fats')
-    print('you have consumed',car,'amount of calorys in carbs')
+    print('you have consumed', fat, 'amount of calorys in fats')
+    print('you have consumed', car, 'amount of calorys in carbs')
+
+# Function to calculate calories from fats (9 calories per gram)
 def fats(caler):
     fat = caler * 9
     return fat
+
+# Function to calculate calories from carbs (4 calories per gram)
 def carb(cal):
     car = cal * 4
     return car
 
+
+# Function for Exercise 3: Ticket sales calculation for different classes
 def ex3():
+    # Call functions for each ticket class and compute total revenue
     aticp = aclass()
     bticp = bclass()
     cticp = cclass()
+    
+    # Calculate total revenue
     tot = aticp + bticp + cticp
-    print('you have made a total of',tot,'$')
+    print('you have made a total of', tot, '$')
+
+# Function to get input for A-class ticket sales (20 per ticket)
 def aclass():
     atic = float(input('how many a seat ticets sold'))
     if atic < 0:
         print('enter a valid number')
-        ex3()
+        ex3()  # Recursively call ex3 if input is invalid
     else:
         aticp = atic * 20
         return aticp
+
+# Function to get input for B-class ticket sales (15 per ticket)
 def bclass():
     btic = float(input('how many b seat ticets sold'))
     if btic < 0:
@@ -89,6 +126,8 @@ def bclass():
     else:
         bticp = btic * 15
         return bticp
+
+# Function to get input for C-class ticket sales (10 per ticket)
 def cclass():
     ctic = float(input('how many c seat ticets sold'))
     if ctic < 0:
@@ -97,31 +136,107 @@ def cclass():
     else:
         cticp = ctic * 10
         return cticp
-    
+
+
+# Function for Exercise 4: Calculates painting project cost (including labor)
 def ex4():
+    # Take input for the size of the wall and cost of paint
     wall = float(input('how big is the wall'))
     cost = float(input('how much per gallon of paint'))
+    
+    # Call helper functions to calculate the cost of paint and labor
     galcost = gallion(wall, cost)
     labcost = laber(galcost, cost)
+    
+    # Calculate the total cost (paint + labor)
     totalcost = totalll(labcost, galcost)
-    print('the cost of paint for this project is',galcost,'$')
-    print('the cost for laber is',labcost,'$')
+    
+    # Output the results
+    print('the cost of paint for this project is', galcost, '$')
+    print('the cost for laber is', labcost, '$')
+
+# Function to calculate paint cost based on wall size and cost per gallon
 def gallion(wall, cost):
-    galneed = wall / 112
-    galgal = math.ceil(galneed)
-    galcost = galgal * cost
+    galneed = wall / 112  # Assuming 1 gallon covers 112 sq ft
+    galgal = math.ceil(galneed)  # Round up to the nearest gallon
+    galcost = galgal * cost  # Total cost for paint
     return galcost
+
+# Function to calculate labor cost (based on paint cost)
 def laber(galcost, cost):
-    labb = galcost / cost
-    lab = labb * 8
-    labcost = lab * 35
+    labb = galcost / cost  # Number of hours needed based on gallons
+    lab = labb * 8  # 8 hours of work per gallon
+    labcost = lab * 35  # Cost of labor (35 per hour)
     return labcost
+
+# Function to calculate total cost (paint cost + labor cost)
 def totalll(labcost, galcost):
     totalcost = labcost + galcost
     return totalcost
 
 def ex5():
-    #make them pick sumthing call thats task
+    #calls generater
+    number1, number2 = numbrr()
+    #sets anser
+    number3 = number1 + number2
+    print('solve')
+    print('',number1)
+    print('+',number2)
+    #asks for the anser
+    see = int(input('answer:'))
+    #checks the anser
+    if see == number3:
+        print('corect')
+        tr = input('do you want to try agion y/n')
+        if tr == 'y':
+            ex5()
+        else:
+            print('ending')
+    else:
+        print('no the anser was',number3)
+        tr = input('do you want to try agion y/n')
+        if tr == 'y':
+            ex5()
+        else:
+            print('ending')
+    #generates the number
+def numbrr():
+    number1 = random.randint(1, 200)
+    number2 = random.randint(1, 200)
+    return number1, number2
+
+def ex6():
+    loop()
+def loop():
+    time = 0
+    while time < 10:
+        time = time + 1
+        falling(time)
+def falling(time):
+    des = .5 * 9.8 * time ** 2
+    print(format(time,',.2f'),'sec',format(des, ',.2f'),'m')
+    
+# Function for Exercise 5: A Rock, Paper, Scissors, Lizard, Spock game
+def ex7():
+    # Ask the user to choose their weapon
+    ch = input('select your wepion: rock, paper, sissers, lizzard, spock')
+    
+    # Call the corresponding function based on the user's choice
+    if ch == 'spock':
+        spock()
+    elif ch == 'sissers':
+        sisers()
+    elif ch == 'rock':
+        rock()
+    elif ch == 'paper':
+        paper()
+    elif ch == 'lizzard':
+        lizzard()
+    elif ch == 'nuke':
+        nuke()
+    else:
+        print('not a option')
+
     ch = input('select your wepion: rock, paper, sissers, lizzard, spock')
     if ch == 'spock':
         spock()
@@ -143,7 +258,7 @@ def spock():
         print('computer chose rock')
         print('you win')
         go = input('do you want to play agion Y/N')
-        if go == 'Y':
+        if go == 'y':
             ex5()
         else:
             print('ending')
@@ -151,7 +266,7 @@ def spock():
         print('computer chose paper')
         print('you loss')
         go = input('do you want to play agion Y/N')
-        if go == 'Y':
+        if go == 'y':
             ex5()
         else:
             print('ending')
@@ -159,7 +274,7 @@ def spock():
         print('computer chose sizzers')
         print('you win')
         go = input('do you want to play agion Y/N')
-        if go == 'Y':
+        if go == '':
             ex5()
         else:
             print('ending')
@@ -167,7 +282,7 @@ def spock():
         print('computer chose lizzard')
         print('you loss')
         go = input('do you want to play agion Y/N')
-        if go == 'Y':
+        if go == 'y':
             ex5()
         else:
             print('ending')
@@ -351,13 +466,19 @@ def lizzard():
 def nuke():
     print('you win')
 
-def ex6():
+# Functions for Scissors, Paper, Lizard, and Nuke have similar structure.
+
+
+# Function for Exercise 6: Draws a stick figure using a graphical library
+def ex8():
+    #calls the makers
     bace()
     mid()
     head()
     arm()
     hat()
     face()
+    #makes them
 def bace():
     my_gragh.circle(-50, -35, 80, 'blue')
 def mid():
@@ -383,10 +504,13 @@ def face():
     my_gragh.square(20, 190, 6, 'brown')
     my_gragh.circle(20, 210, 5, 'gray')
     my_gragh.circle(20, 230, 5, 'gray')
-    
-def ex7():
+
+# Function for Exercise 7: Draws alternating black and white squares
+def ex9():
     tt = 0
     printer(tt)
+
+# Function to print alternating black and white squares
 def printer(tt):
     cut = 2
     gim = 1
@@ -403,7 +527,8 @@ def printer(tt):
         y = y - 50
         pp = 0
         x = -50
-        
+
+# Function to alternate between black and white colors
 def color(cut):
     if cut == 1:
         collor = 'white'
@@ -414,4 +539,4 @@ def color(cut):
         collor = 'black'
         return collor, cut
     else:
-        print('utooh')
+        print('oops')
