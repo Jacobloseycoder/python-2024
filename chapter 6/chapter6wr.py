fe296e8f746b73a8b65c101c91ca9cfb7d090792
Jacobloseycoder <loseyjacob1@gmail.com>
2024-12-10 13:30:14 -0600

    Update chapter6wr.py

--- chapter 6/chapter6wr.py
+++ chapter 6/chapter6wr.py
@@ -1,4 +1,10 @@
 import os
 import menu
 def w1():
-    
+    number = 0
+    while number < 365:
+        infile = open('steps.txt','r')
+        lines = infile.readline
+        number = number + 1
+        print(number,':   ',lines)
+        infile.close()
