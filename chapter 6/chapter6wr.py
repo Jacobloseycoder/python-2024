cebfdbd50d0d5567483a0c07586bf7cb906b19c4
Jacobloseycoder <loseyjacob1@gmail.com>
2024-12-13 13:52:14 -0600

    Update chapter6wr.py

--- chapter 6/chapter6wr.py
+++ chapter 6/chapter6wr.py
@@ -2,17 +2,21 @@ import os
 import menu
 import random
 def w1():
-    number = 1
-    #opens file
-    infile = open('steps.txt','r')
-    lines = infile.readline()
-    #prints the steps and days
-    while lines != '':
-        print(number,':   ',lines)
-        number = number + 1
+    try:
+        number = 1
+        #opens file
+        file = input('select a file:')
+        infile = open(file,'r')
         lines = infile.readline()
-    #closes the file
-    infile.close()
+        #prints the steps and days
+        while lines != '':
+            print(number,':   ',lines)
+            number = number + 1
+            lines = infile.readline()
+        #closes the file
+        infile.close()
+    except IOError:
+        print('file is not hear.')
     
 def w2():
     #asks for the file to open
@@ -69,27 +73,28 @@ def w3():
         print('file is not hear.')
         
 def w4():
-    #test
+    #cam the brogect
     gogo = int(0)
-    gen = int(input("how many numbers do you want to make:"))
     try:
+        gen = int(input("how many numbers do you want to make:"))
         infile = open('random.txt','w')
         while gogo < gen:
             mom = str(random.randrange(1, 500))
-            infile.write(mom)
+            infile.write(mom + '\n')
             gogo = gogo + 1
-            print('all numbers writen')
+        print('all numbers writen')
         infile.close()
-    except IOError:
-        print('file is not hear.')
+    except ValueError:
+        print('not a number try agion.')
+        w4()
         
 def w5():
-    #test
+    #cam the brogect
     tot = 0
     tot = int(tot)
     num = 0
     num = int(num)
-    infile = open(random.txt, 'r')
+    infile = open('random.txt', 'r')
     lines = infile.readline()
     lines = lines.rstrip('\n')
     while lines != '':
@@ -99,4 +104,32 @@ def w5():
         print(lines)
         lines = infile.readline()
         lines = lines.rstrip('\n')
-    print('the total the',num,'random numbers is:',tot)
+    print('the total of the',num,'random numbers is:',tot)
+
+def w6():
+    try:
+        print('1: read data')
+        print('2: append data')
+        print('3: exit')
+        sele = input('selet here:')
+        if sele == 1:
+            infile = open('golff.txt', 'r')
+            geff = infile.read()
+            infile.close()
+            print(geff)
+        elif sele == 2:
+            rere = 'y'
+            infile = open('golff.txt', 'w')
+            while rere == 'y' or rere == 'Y':
+                name = input('what is the name:')
+                score = input('what is the score:')
+                infile.write(name + '/n')
+                infile.write(score + '/n')
+                rere = input('do you want to add another y/n:')
+            infile.close()
+        elif sele == 3:
+            print('ending program...')
+        else:
+            print('not a choose')
+    except IOError:
+        print('file is not hear.')
