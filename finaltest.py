410208fe019c4d8c0f3aba8d4378b114783b464c
Jacobloseycoder <loseyjacob1@gmail.com>
2024-12-17 13:19:07 -0600

    Update finaltest.py

--- finaltest.py
+++ finaltest.py
@@ -5,8 +5,8 @@ def start():
     fif = input('select here:')
     if fif == '1':
         name = input('what is your name:')
-        infile = open('anseseses.txt','a+')
-        infile.write(name + '/n')
+        infile = open('anseseses.txt','w')
+        infile.write(name + '\n')
         infile.close()
         quiz()
     elif fif == '2':
@@ -30,8 +30,9 @@ def quiz():
                     if jil == true:
                         print('good job')
 def q1():
+    an = 0
     print('what is 1+1')
-    while an != 2:
+    while an != 'b':
         print('a:1')
         print('b:2')
         print('c:3')
@@ -42,13 +43,14 @@ def q1():
             q1()
         else:
             infile = open('anseseses.txt')
-            infile.write(an + '/n')
+            infile.write(an + '\n')
             infile.close()
     return(true)
 
 def q2():
+    an = 0
     print('who is a sigma')
-    while an != 2:
+    while an != 'c':
         print('a:jimmy')
         print('b:timmy')
         print('c:mr.hays')
@@ -59,13 +61,14 @@ def q2():
             q2()
         else:
             infile = open('anseseses.txt')
-            infile.write(an + '/n')
+            infile.write(an + '\n')
             infile.close()
     return(true)
 
 def q3():
+    an = 0
     print('who wins 1 billion lions or 1 of every pokemon')
-    while an != 2:
+    while an != 'b':
         print('a:lions')
         print('b:pokemon')
         an = input('what is the anser:')
@@ -74,13 +77,14 @@ def q3():
             q3()
         else:
             infile = open('anseseses.txt')
-            infile.write(an + '/n')
+            infile.write(an + '\n')
             infile.close()
     return(true)
 
 def q4():
+    an = 0
     print('what is pie')
-    while an != 2:
+    while an != 'a':
         print('a:food')
         print('b:3.14')
         print('c:12')
@@ -91,13 +95,14 @@ def q4():
             q4()
         else:
             infile = open('anseseses.txt')
-            infile.write(an + '/n')
+            infile.write(an + '\n')
             infile.close()
     return(true)
 
 def q5():
+    an = 0
     print('how many months have 28 days')
-    while an != 2:
+    while an != 'b':
         print('a:1')
         print('b:all')
         print('c:3')
@@ -108,7 +113,7 @@ def q5():
             q5()
         else:
             infile = open('anseseses.txt')
-            infile.write(an + '/n')
+            infile.write(an + '\n')
             infile.close()
     return(true)
 
