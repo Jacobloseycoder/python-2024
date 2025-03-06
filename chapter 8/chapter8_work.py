40539c85c83f2fbb30ea4daa348bff321931b3a8
jacob losey <loseyjacob1@gmail.com>
2025-03-06 15:10:38 -0600

    Update chapter8_work.py

--- chapter 8/chapter8_work.py
+++ chapter 8/chapter8_work.py
@@ -15,18 +15,22 @@ def w1():
         
 def w2():
     month = 0
+    day = 0
     string = input("enter a date in mm/dd/yy format")
     if string[0].isdigit():
-        if string[0] < 2 and string[0] > -1:
-            if srtring[0] == 1
-            month = month + 10
+        if string[0] == 0 or string[0] == 1:
+            if srtring[0] == 1:
+                month = month + 10
             if string[1].isdigit():
+                month = month + string[1]
                 if string[2] == '/':
                     if string[3].isdigit():
-                        if string[4].isdigit():
-                            if string[5].isdigit():
-                                if string[6].isdigit():
-                                    if string[7].isdigit():
-                                        if string[8].isdigit():
-                                            if string[9].isdigit():
-                                                if string[-1] == string[0]:
+                        if string[3] > -1 and string[3] < 4
+                            day = string[3] * 10
+                            if string[4].isdigit():
+                                if string[5] == '/':
+                                    if string[6].isdigit():
+                                        if string[7].isdigit():
+                                            if string[8].isdigit():
+                                                if string[9].isdigit():
+                                                    if string[-1] == string[0]:
