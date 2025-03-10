b34159b61e48af899e940968202b835d9e55e04c
Jacobloseycoder <loseyjacob1@gmail.com>
2025-03-10 13:29:25 -0500

    Create menu.py

---
+++ chapter 8/menu.py
@@ -0,0 +1,12 @@
+def main333():
+    print('please select from the list')
+    print('1. digit string math')
+    print('2. data converter')
+    print('3. morce code')
+    print('4. telephone translater')
+    print('5. avrige number of words')
+    print('6. pig lation')
+    print('7. power ball')
+    print('8. gas')
+    choice = int(input('enter choice:'))
+    return choice
