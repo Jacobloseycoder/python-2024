c3b37acd3ca5525ed8be1edc36a42c1365abd13a
Jacobloseycoder <loseyjacob1@gmail.com>
2025-04-03 13:24:51 -0500

    Update chapter 9 work.py

--- chapter 9/chapter 9 work.py
+++ chapter 9/chapter 9 work.py
@@ -1,5 +1,10 @@
 def w1():
-    coded = open('bill.txt')
-    secrit = {coded}
-    print(secrit)
+    coded = open('bill.txt','r')
+    encode = open('code.txt','r')
+    lines = coded.readline()
+    enlines = encode.readlines()
+    secrit = {enlines}
+    print(lines)
+    print(enlines)
     coded.close()
+    encode.close()
