642c0f48aae4bfbd7480b8c29512527078379ef8
Jacobloseycoder <loseyjacob1@gmail.com>
2025-04-04 13:41:15 -0500

    Update chapter 9 work.py

--- chapter 9/chapter 9 work.py
+++ chapter 9/chapter 9 work.py
@@ -1,12 +1,22 @@
 def w1():
     dic = {}
+    go = True
+    #the messige to decode
     coded = open('bill.txt', 'r')
+    #the .txt file with the way to encript
     encode = open('code.txt', 'r')
     for line in encode:
         line = line.strip()
-        if ":" in line:
-            key, value = line.split(":", 1)
-            dic[key] = value
+        key, value = line.split(":", 1)
+        dic[key] = value
+    print(dic)
+    while go == True:
+        line = coded.read(1)
+        line = line.lower()
+        if line != '':
+            micc = dic[line]
+        else:
+            micc += ''
     coded.close()
     encode.close()
-    print(dic)
+    print(micc)
