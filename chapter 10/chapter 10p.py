5ce0d3a2e8f3beefd7aa3c31804e598e746a9673
Jacobloseycoder <loseyjacob1@gmail.com>
2025-04-14 13:51:40 -0500

    Update chapter 10p.py

---
+++ chapter 10/chapter 10p.py
@@ -0,0 +1,32 @@
+class dog:
+    species = 'canis familiaris'
+    def __init__(self, name, age):
+        self.name = name;
+        self.age = age;
+#enter varibal = class('name', age)
+#varibal.name or varibal.age
+    def description(self):
+        return f"{self.name} is {self.age} year old."
+#enter variable.description()
+    def speak(self, sound):
+        return f"{self.name} says {sound}."
+#enter variable.speak('messige')
+    def __str__(self):
+        return f"{self.name} is {self.age} years old."
+    #enter variable.str()
+    def bread(self, breed):
+        return f"{self.name} is a {breed}."
+    #enter variable.bread('messige')
+    
+import random
+
+class coin:
+    def __init__(self):
+        self.__sideup = 'heads'
+    def toss(self):
+        if random.randent(0,1) == 0:
+            self.__sideup = 'heads'
+        else:
+            self.__sideup = 'tails'
+    def get_sideup(self):
+        return self.__sideup
