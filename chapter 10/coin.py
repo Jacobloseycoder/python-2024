0b1d0d931e6db9f88bc7b0e3bdd84a6029aabdc6
Jacobloseycoder <loseyjacob1@gmail.com>
2025-04-15 13:33:02 -0500

    Create coin.py

---
+++ chapter 10/coin.py
@@ -0,0 +1,10 @@
+class coin:
+    def __init__(self):
+        self.__sideup = 'heads'
+    def toss(self):
+        if random.randent(0,1) == 0:
+            self.__sideup = 'heads'
+        else:
+            self.__sideup = 'tails'
+    def get_sideup(self):
+        return self.__sideup
