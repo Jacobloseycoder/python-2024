b79041bb7b3249b1cc41bb36222df9af495331b9
Jacobloseycoder <loseyjacob1@gmail.com>
2025-04-29 13:57:11 -0500

    Update chapter 10w.py

--- chapter 10/chapter 10w.py
+++ chapter 10/chapter 10w.py
@@ -34,21 +34,56 @@ def add_pet():
     t = input('enter the pets name')
     tt = input('enter the pets type')
     ttt = input('enter the pets age')
-    t = pet.(t, tt, ttt)
+    pet_list = []
+    pet_list.append(t)
+    t = pet(t, tt, ttt)
+
 def modify_pet():
     print('1. change name')
     print('2. change age')
     print('3. change type')
     go = input('enter here:')
     if go == 1:
-        
+        print('enter the origanal name')
+        og = input('enter here:')
+        print('enter the new name')
+        new = input('enter here:')
+        name = new
+        age = og.get_age()
+        typee = og.get_type()
+        del pet.og
+        name = pet(name, age, typee)
     elif go == 2:
-        
+        print('enter the name')
+        og = input('enter here:')
+        print('enter the new age')
+        new = input('enter here:')
+        name = og
+        age = new
+        typee = og.get_type()
+        del pet.og
+        name = pet(name, age, typee)
     elif go == 3:
+        print('enter the name')
+        og = input('enter here:')
+        print('enter the new type')
+        new = input('enter here:')
+        name = og
+        age = og.get_age()
+        typee = new
+        del pet.og
+        name = pet(name, age, typee)
         
 def display_pet():
+    print('enter the name of the pet you want to see')
+    pett = input('enter here')
+    name = pett.get_name
+    age = pett.get_age
+    typee = pett.get_type
+    print(f'{name} {age} {type}')
     
 def display_all():
-    
+    pass
+
 def qquit():
     print('ending...')
